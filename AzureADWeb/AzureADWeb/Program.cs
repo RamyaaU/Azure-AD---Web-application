62e422b7d0f34c5925c8c3ae1551ba8aa437309f
Ramya <ramyayadav2016@gmail.com>
2024-01-19 16:50:17 +0530

    corrected client id

--- AzureADWeb/AzureADWeb/Program.cs
+++ AzureADWeb/AzureADWeb/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 var builder = WebApplication.CreateBuilder(args);
 
 //application id - 3145f25f-fecb-4ea2-b291-7a035ce927d2
+//client id - e4b72a16-7c0b-449e-b7ee-f7e510c2daab
 //oauthid - https://azureeADLearning.b2clogin.com/azureeADLearning.onmicrosoft.com/<policy-name>/oauth2/v2.0/token
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -16,7 +17,7 @@ builder.Services.AddAuthentication(options =>
 {
     options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
     options.Authority = "https://login.microsoftonline.com/3145f25f-fecb-4ea2-b291-7a035ce927d2/v2.0";
-    options.ClientId = "3145f25f-fecb-4ea2-b291-7a035ce927d2";
+    options.ClientId = "e4b72a16-7c0b-449e-b7ee-f7e510c2daab";
     options.ResponseType = "id_token";
     options.SaveTokens = true;
 });
@@ -36,7 +37,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthentication();
+app.UseAuthentication(); 
 app.UseAuthorization();
 
 app.MapControllerRoute(
--- AzureADWeb/AzureADWeb/obj/Debug/net6.0/AzureADWeb.AssemblyInfo.cs
+++ AzureADWeb/AzureADWeb/obj/Debug/net6.0/AzureADWeb.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AzureADWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de7f9a093ad30797dbcea1e846a4d9d1cbd2a2bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+537d89227d77cf5a032ad5de9cabdc7f3a3b7228")]
 [assembly: System.Reflection.AssemblyProductAttribute("AzureADWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AzureADWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
